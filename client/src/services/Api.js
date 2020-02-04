11b78909a48f639a92dc6ab9f6db3a2c5beed9b6
overlorddamygod <overlord.damygod@gmail.com>
2020-02-04 18:49:36 +0545

    lol

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: `https://localhost:3000`
+    baseURL: `http://localhost:3000`
     // headers: {
     //   Authorization: `Bearer ${store.state.auth.token}`
     // },
